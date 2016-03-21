9f131a63a23507a54ad36f7b06073c8dc6387584
CodyKochmann <kochmanncody@gmail.com>
2016-03-21 15:06:46 -0400

    cleared the __init__.py

--- software/cluster_db/snippets/__init__.py
+++ software/cluster_db/snippets/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # @Author: CodyKochmann
-# @Date:   2016-03-21 14:54:47
-# @Last Modified by:   CodyKochmann
-# @Last Modified time: 2016-03-21 14:54:47
+# @Date:   2016-03-21 15:06:30
+# @Last Modified 2016-03-21me>
+# @Last Modified time: 2016-03-21 15:06:46
