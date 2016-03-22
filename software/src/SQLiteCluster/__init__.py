c6abae5e7b7a98911e420234c5f847fcf76e02df
CodyKochmann <kochmanncody@gmail.com>
2016-03-22 01:15:56 -0400

    made cleaner database autocreation and added it to the __init__ script

--- software/src/SQLiteCluster/__init__.py
+++ software/src/SQLiteCluster/__init__.py
@@ -2,9 +2,11 @@
 # @Author: CodyKochmann
 # @Date:   2016-03-21 16:41:46
 # @Last Modified 2016-03-22
-# @Last Modified time: 2016-03-22 00:53:44
+# @Last Modified time: 2016-03-22 01:14:45
 
 from get_database_names import get_database_names
 from get_next_database import get_next_database
 from build_local_dbs import build_local_dbs
 
+# ensure the databases are already generated
+build_local_dbs()
--- software/src/run.py
+++ software/src/run.py
@@ -2,9 +2,8 @@
 # @Author: CodyKochmann
 # @Date:   2016-03-21 16:50:58
 # @Last Modified 2016-03-22
-# @Last Modified time: 2016-03-22 00:58:32
+# @Last Modified time: 2016-03-22 01:15:18
 
 import SQLiteCluster
 
 print(SQLiteCluster.get_next_database())
-print(SQLiteCluster.build_local_dbs())
