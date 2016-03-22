3bb912df8460cda289685d86909b7f1cacbc6512
CodyKochmann <kochmanncody@gmail.com>
2016-03-22 02:31:09 -0400

    added a select interface for the databases stored locally to be globally searched

--- software/src/SQLiteCluster/select.py
+++ software/src/SQLiteCluster/select.py
@@ -2,7 +2,7 @@
 # @Author: CodyKochmann
 # @Date:   2016-03-22 01:17:44
 # @Last Modified 2016-03-22
-# @Last Modified time: 2016-03-22 01:54:59
+# @Last Modified time: 2016-03-22 02:26:21
 
 # This queries every database on the local 
 # machine and returns the compiled result. 
@@ -11,22 +11,21 @@ from snippets import *
 from get_database_names import *
 import sqlite3
 
-def run_query(sql="",db_path=""):
-    if valid_select_query(sql) 
-        conn = sqlite3.connect(db_path)
-        with conn:
-            cur = conn.cursor()
-            cur.execute(sql)
-            return(cur.fetchall())
+def run_select_query(sql,db_path):
+    if valid_select_query(sql):
+        result = run_query(sql, db_path)
 
 # queries every database local to this machine and 
 # joins the results into one array
-def query_all(select_query):
+def select(select_query):
     output = False
     if valid_select_query(sql):
         output = []
         for i in get_database_names():
-            for ii in run_query(select_query,i):
-                output.append(ii)
+            try:
+                for ii in run_select_query(select_query,i):
+                    output.append(ii)
+            except:
+                pass
     return(output)
 
