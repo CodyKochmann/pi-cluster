ec2c1bea5f04b5516538ac0a857c0dee3093e235
CodyKochmann <kochmanncody@gmail.com>
2016-03-22 14:49:00 -0400

    added the source for cherrypy usage in the project

--- software/src/run.py
+++ software/src/run.py
@@ -2,8 +2,10 @@
 # @Author: CodyKochmann
 # @Date:   2016-03-21 16:50:58
 # @Last Modified 2016-03-22
-# @Last Modified time: 2016-03-22 01:15:18
+# @Last Modified time: 2016-03-22 14:29:18
 
 import SQLiteCluster
 
 print(SQLiteCluster.get_next_database())
+
+import cherrypy
