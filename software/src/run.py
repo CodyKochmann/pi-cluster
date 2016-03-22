cd0794ee117e9192e3fdc6703c15499f2d459412
CodyKochmann <kochmanncody@gmail.com>
2016-03-22 01:13:13 -0400

    ran successful tests of the database builder and added it to the gitignore file

--- software/src/run.py
+++ software/src/run.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # @Author: CodyKochmann
 # @Date:   2016-03-21 16:50:58
-# @Last Modified 2016-03-21
-# @Last Modified time: 2016-03-21 17:57:57
+# @Last Modified 2016-03-22
+# @Last Modified time: 2016-03-22 00:58:32
 
 import SQLiteCluster
 
-print(SQLiteCluster.get_database_names())
 print(SQLiteCluster.get_next_database())
+print(SQLiteCluster.build_local_dbs())
