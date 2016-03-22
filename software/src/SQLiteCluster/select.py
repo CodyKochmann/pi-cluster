236d8931a7439d67101311feed758d361c36bf86
CodyKochmann <kochmanncody@gmail.com>
2016-03-22 15:38:51 -0400

    normalized the select query handling since the db_name is easy to access now

--- software/src/SQLiteCluster/select.py
+++ software/src/SQLiteCluster/select.py
@@ -2,7 +2,7 @@
 # @Author: CodyKochmann
 # @Date:   2016-03-22 01:17:44
 # @Last Modified 2016-03-22
-# @Last Modified time: 2016-03-22 02:26:21
+# @Last Modified time: 2016-03-22 15:26:50
 
 # This queries every database on the local 
 # machine and returns the compiled result. 
@@ -11,9 +11,9 @@ from snippets import *
 from get_database_names import *
 import sqlite3
 
-def run_select_query(sql,db_path):
+def run_select_query(sql,db_name):
     if valid_select_query(sql):
-        result = run_query(sql, db_path)
+        result = run_query(sql, db_name)
 
 # queries every database local to this machine and 
 # joins the results into one array
