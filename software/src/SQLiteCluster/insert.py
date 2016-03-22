dc64ad5a3d72cb14d81e980797282205c22adaa6
CodyKochmann <kochmanncody@gmail.com>
2016-03-22 15:39:55 -0400

    cleaned up the validation and added the db_name logic into the insert service

--- software/src/SQLiteCluster/insert.py
+++ software/src/SQLiteCluster/insert.py
@@ -2,16 +2,15 @@
 # @Author: CodyKochmann
 # @Date:   2016-03-22 02:02:36
 # @Last Modified 2016-03-22
-# @Last Modified time: 2016-03-22 02:23:02
+# @Last Modified time: 2016-03-22 15:25:00
 
 # runs insert queries if they apply to that machine
 
 from snippets import *
 from get_database_names import *
+from run_query import *
 
-def insert(db_path,insert_sql):
-    if "db/" not in db_path:
-        db_path="db/%s"%(db_path)
-    if db_path in get_database_names(): 
+def insert(db_name,sql):
+    if db_name in get_database_names(): 
         if valid_insert_query(sql):
-            return(run_query(sql, db_path))
+            return(run_query(sql, db_name))
