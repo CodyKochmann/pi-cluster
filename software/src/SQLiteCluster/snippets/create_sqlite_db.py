9422eb4831a4f645931f312f7a95128e48c64361
CodyKochmann <kochmanncody@gmail.com>
2016-03-22 14:47:32 -0400

    added logging to the sqlite database generator

--- software/src/SQLiteCluster/snippets/create_sqlite_db.py
+++ software/src/SQLiteCluster/snippets/create_sqlite_db.py
@@ -1,7 +1,14 @@
+
+from log import *
+
 # creates a sqlite database
 def create_sqlite_db(db_path):
+    log("building : %s"%(db_path))
     import sqlite3
-    conn = sqlite3.connect(db_path)
-    conn.commit()
-    conn.close()
+    try:
+        conn = sqlite3.connect(db_path)
+        conn.commit()
+        conn.close()
+    except Exception, e:
+        log(e)
     return
